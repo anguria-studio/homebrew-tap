00a8f9132d064e42e29542634fa23378a26a0030
tommasoltrz <tommasoltrz@gmail.com>
2026-09-23 14:08:31 +0200

    Update Paguro to 1.0.8 (17)

--- Casks/paguro.rb
+++ Casks/paguro.rb
@@ -1,6 +1,6 @@
 cask "paguro" do
-  version "1.0.7,16"
-  sha256 "3ee112b792c2a2ec858e751a2f97e5e27afa7b42722d1cb72b738a767a100be2"
+  version "1.0.8,17"
+  sha256 "cb541a6d75d43631bfefb395ab4d6aa376c9ea853e2551deaf843bb72a60aca7"
 
   url "https://github.com/anguria-studio/Paguro/releases/download/v#{version.csv.first}/Paguro-#{version.csv.first}-#{version.csv.second}.dmg"
   name "Paguro"
